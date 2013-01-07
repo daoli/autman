96366e73ee01f9e452b0018485b9115840c0f972
Daoyuan Li <daoyuan.li@yabroad.com>
2013-01-07 13:13:32 +0800

    Second try.

--- lunch/reservation/views.py
+++ lunch/reservation/views.py
@@ -3,20 +3,27 @@ from reservation.models import Reservation
 from reservation.models import UserProfile
 from django.http import Http404
 from django.http import HttpResponse
+from django.utils import timezone
 
 def reserve(request):
-	user = request.user
-	profile = None
-	try:
-		profile = UserProfile.objects.get(user=user)
-	except:
-		pass
-	if profile:
-		if profile.balance < 10:
-			return HttpResponse('Balance less than 10 CNY, please recharge')
-		else:
-			profile.balance -= 10
-			profile.save()
-			Reservation.objects.create(user=user)
-			return HttpResponse('OK!')
-	raise Http404()
+    user = request.user
+    profile = None
+    try:
+        profile = UserProfile.objects.get(user=user)
+    except:
+        pass
+    if profile:
+        if profile.balance < 10:
+            return HttpResponse('Balance less than 10 CNY, please recharge')
+        else:
+            date = timezone.now().date
+            reservation = Reservation.objects.filter(user=user, date=date)
+            if reservation:
+                # already reserved
+                return HttpResponse('You already reserved lunch, thanks!')
+            profile.balance -= 10
+            profile.save()
+            Reservation.objects.create(user=user)
+            msg = 'OK! Your reservation has been recorded. You balance is %s CNY.' % profile.balance
+            return HttpResponse(msg)
+    raise Http404()
