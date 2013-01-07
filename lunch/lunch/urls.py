553b64424c54edd9c9ba41ff5f8d0c54341cbce7
Xin Gu <xin.gu@yabroad.com>
2013-01-07 16:02:26 +0800

    dfierer

--- lunch/lunch/urls.py
+++ lunch/lunch/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 admin.autodiscover()
 
@@ -11,4 +13,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^$', lambda x: HttpResponseRedirect(reverse('resv:reservation')))
 )
--- lunch/reservation/views.py
+++ lunch/reservation/views.py
@@ -1,7 +1,5 @@
 from reservation.models import Reservation
 from reservation.models import UserProfile
-from django.http import Http404
-from django.http import HttpResponse
 from django.utils import timezone
 from django.views.generic import View
 from django.template import RequestContext
@@ -12,31 +10,40 @@ from django.core.urlresolvers import reverse
 
 
 class ReservationView(View):
+    def get_information(self, request):
+        data = {}
+        user = request.user
+        profile = UserProfile.objects.get(user=user)
+
+        if profile.balance < 10:
+            msg = 'Balance less than 10 CNY, please recharge'
+            data['msg'] = msg
+
+        date = timezone.now().date
+        reservations = Reservation.objects.filter(user=user, date=date)
+
+        data.update({
+            'profile': profile,
+            'count_today': reservations.count(),
+        })
+
+        return data
+
     def get(self, request, *args, **kwargs):
         return render_to_response('reservation/index.html',
-                              {},
+                              self.get_information(request),
                               context_instance=RequestContext(request))
 
     def post(self, request, *args, **kwargs):
         user = request.user
-        profile = None
-        try:
-            profile = UserProfile.objects.get(user=user)
-        except:
-            pass
-        if profile:
-            if profile.balance < 10:
-                return HttpResponse('Balance less than 10 CNY, please recharge')
-            else:
-                profile.balance -= 10
-                profile.save()
-                Reservation.objects.create(user=user)
 
-                date = timezone.now().date
-                reservations = Reservation.objects.filter(date=date)
-                msg = 'OK! Your reservation has been recorded. You balance is %s CNY. You ordered %s meals today.' % (profile.balance, reservations.count())
-                return HttpResponse(msg)
-        raise Http404()
+        profile = UserProfile.objects.get(user=user)
+        if profile.balance >= 10:
+            profile.balance -= 10
+            profile.save()
+            Reservation.objects.create(user=user)
+
+        return HttpResponseRedirect(reverse('resv:reservation', kwargs={}))
 
 
 def login_user(request):
@@ -58,6 +65,5 @@ def login_user(request):
         else:
             state = "Your username and/or password were incorrect."
 
-    print state
-    return render_to_response('reservation/login.html',{'state':state, 'username': username},
+    return render_to_response('reservation/login.html', {'state': state, 'username': username},
         context_instance=RequestContext(request))
