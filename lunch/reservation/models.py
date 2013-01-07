01072fc90e8756fc48dd7692f007b076d153dadc
Daoyuan Li <daoyuan.li@yabroad.com>
2013-01-07 14:13:35 +0800

    Lunch reservation beta.

--- lunch/reservation/models.py
+++ lunch/reservation/models.py
@@ -3,14 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Reservation(models.Model):
-	user = models.ForeignKey(User)
-	date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    date = models.DateField(auto_now_add=True)
 
-	def __unicode__(self):
-		name = self.user.first_name if self.user.first_name else self.user.username
-		return name + ' on ' + str(self.date)
+    def __unicode__(self):
+        name = self.user.first_name if self.user.first_name else self.user.username
+        return name + ' on ' + str(self.date)
 
 
 class UserProfile(models.Model):
-	user = models.ForeignKey(User)
-	balance = models.IntegerField(default=0)
+    user = models.ForeignKey(User)
+    balance = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        name = self.user.first_name if self.user.first_name else self.user.username
+        return name
--- lunch/reservation/views.py
+++ lunch/reservation/views.py
@@ -2,10 +2,13 @@ from reservation.models import Reservation
 from reservation.models import UserProfile
 from django.http import Http404
 from django.http import HttpResponse
-# from django.utils import timezone
+from django.utils import timezone
 from django.views.generic import View
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 class ReservationView(View):
@@ -25,14 +28,36 @@ class ReservationView(View):
             if profile.balance < 10:
                 return HttpResponse('Balance less than 10 CNY, please recharge')
             else:
-                # date = timezone.now().date
-                # reservation = Reservation.objects.filter(user=user, date=date)
-                # if reservation:
-                #     # already reserved
-                #     return HttpResponse('You already reserved lunch, thanks!')
                 profile.balance -= 10
                 profile.save()
                 Reservation.objects.create(user=user)
-                msg = 'OK! Your reservation has been recorded. You balance is %s CNY.' % profile.balance
+
+                date = timezone.now().date
+                reservations = Reservation.objects.filter(date=date)
+                msg = 'OK! Your reservation has been recorded. You balance is %s CNY. You ordered %s meals today.' % (profile.balance, reservations.count())
                 return HttpResponse(msg)
         raise Http404()
+
+
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        print user
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+                return HttpResponseRedirect(reverse('resv:reservation', kwargs={}))
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    print state
+    return render_to_response('reservation/login.html',{'state':state, 'username': username},
+        context_instance=RequestContext(request))
