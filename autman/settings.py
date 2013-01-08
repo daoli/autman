8b8c018a648e4803fb1ade35ecf8ae22c9f727d3
Daoyuan Li <daoyuan.li@yabroad.com>
2013-01-08 21:07:31 +0800

    Rename project to autman -- automated manager.
    The reason is that in the future we may add more apps to the system.
    
    To update your current local repo:
    git remote set-url origin git@github.com:daoli/autman.git

--- autman/settings.py
+++ autman/settings.py
@@ -1,9 +1,9 @@
-# Django settings for lunch project.
+# Django settings for autman project.
 import os
 from os.path import abspath
 
 # A trick to get the absolute path of the project
-LUNCH_PROJECT_HOME = abspath(abspath(os.path.dirname(__file__)) + '/../../')
+AUTMAN_PROJECT_HOME = abspath(abspath(os.path.dirname(__file__)) + '/../../')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -65,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = LUNCH_PROJECT_HOME + '/static/'
+STATIC_ROOT = AUTMAN_PROJECT_HOME + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -76,7 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    LUNCH_PROJECT_HOME + '/lunch/static',
+    AUTMAN_PROJECT_HOME + '/lunch/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -107,10 +107,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'lunch.urls'
+ROOT_URLCONF = 'autman.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'lunch.wsgi.application'
+WSGI_APPLICATION = 'autman.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
