8198b0bb568370f4c4c40713920f16df0eec80b9
Daoyuan Li <daoyuan.li@yabroad.com>
2013-01-08 20:38:42 +0800

    Add recharge history support.

--- lunch/reservation/admin.py
+++ lunch/reservation/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from reservation.models import RechargeHistory
 from reservation.models import Reservation
 from reservation.models import UserProfile
 from reservation.forms import ReservationForm
@@ -20,8 +21,23 @@ class UserProfileAdmin(admin.ModelAdmin):
         if form and form.cleaned_data['recharge_amount']:
             recharge_amount = int(form.cleaned_data['recharge_amount'])
             obj.balance += recharge_amount
+            admin = request.user
+            RechargeHistory.objects.create(user=obj.user,
+                                           admin=admin,
+                                           recharge_amount=recharge_amount)
 
         obj.save()
 
+
+class RechargeHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+                    'user',
+                    'recharge_amount',
+                    'admin',
+                    'datetime',
+                    )
+
+
+admin.site.register(RechargeHistory, RechargeHistoryAdmin)
 admin.site.register(Reservation, ReservationAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
--- lunch/reservation/models.py
+++ lunch/reservation/models.py
@@ -18,3 +18,19 @@ class UserProfile(models.Model):
     def __unicode__(self):
         name = self.user.first_name if self.user.first_name else self.user.username
         return name
+
+
+class RechargeHistory(models.Model):
+    user = models.ForeignKey(User, related_name='user')
+    admin = models.ForeignKey(User, related_name='admin')
+    recharge_amount = models.IntegerField()
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        name = self.user.first_name if self.user.first_name else self.user.username
+        admin = self.admin.first_name if self.admin.first_name else self.admin.username
+        return "%s charged CNY %s at %s by %s" % (name,
+                self.recharge_amount, self.datetime, admin)
+
+    class Meta:
+        verbose_name_plural = "recharge history"
