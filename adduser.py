780856447b6320b21230646516b5589f2064f4b3
Daoyuan Li <daoyuan.li@yabroad.com>
2013-01-28 12:45:35 +0800

    Update adduser script.

--- adduser.py
+++ adduser.py
@@ -1,19 +1,22 @@
 from django.contrib.auth.models import User
 import random
 import string
+import sys
 from reservation.models import UserProfile
-
-names = [('username', 'email@example.com'),
-         ]
 
 
 def generate_pw(n=10):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(n))
 
 
-for name, email in names:
+def add_user(name, email):
     pw = generate_pw()
     user = User.objects.create_user(name, email, pw)
-    print name, email, pw
     profile = UserProfile.objects.create(user=user)
+    print 'User %s added with password: %s' % (name, pw)
 
+
+if __name__ == '__main__':
+	name = sys.argv[1]
+	email = sys.argv[2]
+	add_user(name, email)
